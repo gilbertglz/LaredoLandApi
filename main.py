9a73ac5baf53f594bed527237c11d30ff0e32a3a
gilbertglz <74834795+gilbertglz@users.noreply.github.com>
2023-04-17 17:49:39 -0500

    fix(main): minor editing fixes

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify, request
 import pandas as pd
+import urllib.parse
+
 app = Flask(__name__)
 
-@app.route('/api/<Lot>/<Block>/<Subdivision>')
-def hello(Lot, Block,Subdivision):
+@app.route('/Laredo/<Lot>/<Block>/<Subdivision>')
+def hello(Lot, Block, Subdivision):
     df = pd.read_csv('LaredoLandDetailsClean.csv')
     filtered_df = df[(df['LOT'] == Lot) & (df['BLOCK'] == Block) & (df['SUBDIVISIO'] == Subdivision)]
-    return jsonify({'FULLADDRESS': filtered_df['FULLADDRES'].values[0].strip()})
+    finalAddress = filtered_df['FULLADDRES'].values[0].strip()
+    url_address = urllib.parse.quote_plus(finalAddress + ', Laredo, Texas')
+    maps_url = f"https://www.google.com/maps/search/?api=1&query={url_address}"
+    return jsonify({'FULL ADDRESS': finalAddress, 'gmapsUrl': maps_url})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
